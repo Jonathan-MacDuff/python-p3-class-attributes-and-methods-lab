5206861b6e6f3a4a6671082a384f8961a7ba980f
Jonathan MacDuff <jonathan.macduff@outlook.com>
2024-04-20 13:15:14 -0700

    completed lab

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,40 @@
 class Song:
-    pass
+
+    count = 0 
+
+    genres = []
+
+    genre_count = {}
+
+    artists = []
+
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        self.add_count()
+        self.add_genre(genre)
+        self.add_artist(artist)
+        
+
+    @classmethod
+    def add_count(cls):
+        cls.count += 1 
+
+    @classmethod
+    def add_genre(cls, genre):
+        if cls.genres.__contains__(genre):
+            cls.genre_count[genre] += 1
+        else:
+            cls.genres.append(genre)
+            cls.genre_count.update({genre: 1})
+
+    @classmethod
+    def add_artist(cls, artist):
+        if cls.artists.__contains__(artist):
+            cls.artist_count[artist] += 1
+        else:
+            cls.artists.append(artist)
+            cls.artist_count.update({artist: 1})
